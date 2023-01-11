13ac97c98a72ae28d53ab72abd7518454a0a1f49
ts-robih.robihamanto <ts-robih.robihamanto@rakuten.com>
2023-01-11 17:12:42 +0900

    Update Package for SPM

--- Package.swift
+++ Package.swift
@@ -9,20 +9,18 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Howmuch",
             targets: ["Howmuch"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rakutentech/ios-sdkutils.git", .upToNextMajor(from: "4.0.0"))
+        .package(name: "RSDKUtils" ,url: "https://github.com/rakutentech/ios-sdkutils.git", .upToNextMajor(from: "4.0.0")),
     ],
     targets: [
         .target(
             name: "Howmuch",
-            dependencies: [],
-            resources: [
-                .process("Resources")
+            dependencies: [
+                .product(name: "RSDKUtilsMain", package: "RSDKUtils"),
             ]
         ),
     ],
