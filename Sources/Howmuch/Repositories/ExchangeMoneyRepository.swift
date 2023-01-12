36cf887289db9a9227186f6cc6da24f8b304369b
ts-robih.robihamanto <ts-robih.robihamanto@rakuten.com>
2023-01-12 15:00:13 +0900

    Add apikey on repository

--- Sources/Howmuch/Repositories/ExchangeMoneyRepository.swift
+++ Sources/Howmuch/Repositories/ExchangeMoneyRepository.swift
@@ -2,14 +2,26 @@ import Foundation
 
 internal protocol ExchangeMoneyRepositoryType: AnyObject {
     var defaultHttpSessionConfiguration: URLSessionConfiguration { get }
+
+    func saveApiKey(forKey key: String)
+    func getApiKey() -> String?
 }
 
 internal class ExchangeMoneyRepository: ExchangeMoneyRepositoryType {
 
+    private(set) var apiKey: String?
     private(set) var defaultHttpSessionConfiguration: URLSessionConfiguration = {
         let configuration = URLSessionConfiguration.ephemeral
         configuration.timeoutIntervalForRequest = 20
         return configuration
     }()
+
+    func saveApiKey(forKey key: String) {
+        self.apiKey = key
+    }
+
+    func getApiKey() -> String? {
+        return apiKey
+    }
     
 }
