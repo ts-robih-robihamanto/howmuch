08dc0201b26160c868aaf21fd7e11e8e7ed591e8
ts-robih.robihamanto <ts-robih.robihamanto@rakuten.com>
2023-01-12 16:25:05 +0900

    Update repository configuration

--- Sources/Howmuch/Repositories/ExchangeMoneyRepository.swift
+++ Sources/Howmuch/Repositories/ExchangeMoneyRepository.swift
@@ -3,25 +3,25 @@ import Foundation
 internal protocol ExchangeMoneyRepositoryType: AnyObject {
     var defaultHttpSessionConfiguration: URLSessionConfiguration { get }
 
-    func saveApiKey(forKey key: String)
+    func saveApiKey(_ config: HowmuchModuleConfiguration)
     func getApiKey() -> String?
 }
 
 internal class ExchangeMoneyRepository: ExchangeMoneyRepositoryType {
 
-    private(set) var apiKey: String?
+    private(set) var howmuchModuleConfiguration: HowmuchModuleConfiguration?
     private(set) var defaultHttpSessionConfiguration: URLSessionConfiguration = {
         let configuration = URLSessionConfiguration.ephemeral
         configuration.timeoutIntervalForRequest = 20
         return configuration
     }()
 
-    func saveApiKey(forKey key: String) {
-        self.apiKey = key
+    func saveApiKey(_ config: HowmuchModuleConfiguration) {
+        self.howmuchModuleConfiguration = config
     }
 
     func getApiKey() -> String? {
-        return apiKey
+        return howmuchModuleConfiguration?.apiKey
     }
     
 }
