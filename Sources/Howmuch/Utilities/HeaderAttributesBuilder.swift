3bd94098ed64fdec0183435aa45ef7da960bf800
ts-robih.robihamanto <ts-robih.robihamanto@rakuten.com>
2023-02-07 08:23:29 +0800

    remove unneeded throws

--- Sources/Howmuch/Utilities/HeaderAttributesBuilder.swift
+++ Sources/Howmuch/Utilities/HeaderAttributesBuilder.swift
@@ -6,7 +6,7 @@ struct HeaderAttributesBuilder {
     private typealias Keys = Constants.Request.Header
     private(set) var addedHeaders: [HeaderAttribute] = []
 
-    mutating func add(apiKey: String) throws {
+    mutating func add(apiKey: String) {
         addedHeaders.append(HeaderAttribute(key: Keys.apikey, value: apiKey))
     }
 }
